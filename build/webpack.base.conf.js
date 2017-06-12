83f8a76a9d422aa05ac2e4dd3320087ec88e77ab
qiushangzhe <qiusz@jiqdaibao.com>
2017-06-12 18:32:07 +0800

    a

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
@@ -14,46 +14,39 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: process.env.NODE_ENV === 'production' ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '$': path.join(__dirname, '../src/assets/zepto.js'),
       '@': resolve('src')
     }
   },
   module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      options: vueLoaderConfig
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [resolve('src'), resolve('test')]
+    }, {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('img/[name].[hash:7].[ext]')
       }
-    ]
+    }, {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+      }
+    }]
   }
 }
--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,19 +1,37 @@
-import $ from '@/assets/zepto.js'
+import $ from 'jquery'
 export default {
   name: 'HomePage',
-  created: function() {
-      this.picNavBar = $('#homepage-pic-nav');
-      this.initPicNav();
+  mounted: function() {
+    this.picNavBar = $('#homepage-pic-nav .pic-nav');
+    this.initPicNav();
   },
   data() {
     return {
-        picNavBar:null
+      picNavBar: null,
+      picNavX: 0,
+      imgList: [{
+        src: require('@/assets/bg-1.jpg')
+      }, {
+        src: require('@/assets/bg-2.jpg')
+      }],
+      chooseIndex: 0
     }
   },
   methods: {
-      initPicNav:function(){
-
-      }
+    initPicNav: function() {
+      this.picNavBar.css('width', this.picNavBar.children().length * 160 + 'px');
+    },
+    picNavLeft: function() {
+      this.picNavX -= 160;
+      this.picNavBar.css('transform', 'translateX(' + this.picNavX + 'px)');
+    },
+    picNavRight: function() {
+      this.picNavX += 160;
+      this.picNavBar.css('transform', 'translateX(' + this.picNavX + 'px)');
+    },
+    choosePic: function(index) {
+        this.chooseIndex = index;
+    }
   },
   components: {
 
